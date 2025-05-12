a062a853f9fbd5e9a6605fcda55714ec97a623e9
root <root@vmi2595755.contaboserver.net>
2025-05-12 07:51:38 +0200

    v1.9.4

--- goaccess.js
+++ goaccess.js
@@ -1,8 +1,23 @@
-window.addEventListener('load', function() {
-	document.querySelector('.h-dashboard').querySelector('.hidden-xs').innerHTML = 'NGINX - access.log';
+['load', 'focus'].forEach(function(event){
+        window.addEventListener(event, function() {
+                function replaceElements() {
+                        document.querySelector('.h-dashboard').querySelector('.hidden-xs').innerHTML = 'NGINX - access.log';
 
-	const collections = document.querySelectorAll(".dropdown-toggle");
-	collections.forEach(collection => {
-		collection.innerHTML = '<i class="fa fa-gear"></i>';
-	});
+                        const collections = document.querySelectorAll(".dropdown-toggle");
+                        collections.forEach(collection => {
+                                collection.innerHTML = '<i class="fa fa-gear"></i>';
+                        });
+
+                        const collections1 = document.querySelector("#overall").querySelector("ul").querySelectorAll("li");
+                        collections1.forEach(collection => {
+                                collection.classList.remove("col-sm-6");
+                                collection.classList.add("col-xs-6", "col-sm-3");
+                        });
+                }
+
+                replaceElements();
+                // setInterval(function() {
+                //         replaceElements();
+                // }, 1000);
+        });
 });
