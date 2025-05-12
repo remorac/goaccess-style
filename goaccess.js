622a73a1b4528d13092dae905fa5c1c7af7a1e2e
root <root@vmi2595755.contaboserver.net>
2025-05-12 08:18:06 +0200

    js

--- goaccess.js
+++ goaccess.js
@@ -13,7 +13,7 @@
                         const elements1 = document.querySelector("#overall").querySelector("ul").querySelectorAll("li");
                         elements1.forEach(element => {
                                 element.classList.remove("col-sm-6");
-                                element.classList.add("col-xs-6", "col-sm-4");
+                                element.classList.add("col-xs-6", "col-sm-3");
                         });
 
                         const parents0 = document.querySelectorAll("hgroup");
